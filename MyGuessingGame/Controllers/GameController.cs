efb71c118edbd223b4aea442c3d6e1d73103ff58
Beth Douglas <bethDdouglas@gmail.com>
2017-11-14 14:50:20 -0500

    Initial Commit

---
+++ MyGuessingGame/Controllers/GameController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MyGuessingGame.Models;
+
+namespace MyGuessingGame.Controllers
+{
+    public class GameController : Controller
+    {
+        // GET: Game
+        public ActionResult Index()
+        {
+            Session["answer"] = new Random().Next(1, 101);
+            return View();
+        }
+
+        private bool GuessWasCorrect(int guess) =>
+            guess == (int)Session["answer"];
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+
+        public ActionResult Index(Game model)
+        {
+            if (ModelState.IsValid)
+            {
+                ViewBag.Win = GuessWasCorrect(model.Guess);
+            }
+            return View(model);
+        }
+    }
+}
---
+++ MyGuessingGame/Models/Game.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MyGuessingGame.Models
+{
+    public class Game
+    {
+        [Required]
+        [MaxLength(25, ErrorMessage = "Name must be 25 characters or less")]
+        [Display(Name = "Your Name")]
+        public string GuesserName { get; set; }
+
+        [Range(1,100, ErrorMessage = "Please choose a number between 1 and 10.")]
+        public int Guess { get; set; }
+    }
+}
